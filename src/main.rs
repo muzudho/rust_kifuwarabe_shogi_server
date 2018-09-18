2ee07131498567358dac5d9dc3e6738bd354c124
むずでょ <muzudho1@gmail.com>
2018-09-18 23:02:56 +0900

    マルチバイト分からん☆（＾～＾）

--- src/main.rs
+++ src/main.rs
@@ -26,28 +26,29 @@ use std::{thread, time};
 const CONNECTION_STRING: &str = "127.0.0.1:4091";
 
 // クライアントをずっと捕まえておく。
-fn handle_client(mut stream: &TcpStream) {
+fn handle_client(stream: &TcpStream) {
     println!("Capture client.");
     loop {
         // ブロックし続けないようにする。
-        // stream.set_nonblocking(true).expect("set_nonblocking call failed");
+        // let _ = stream.set_read_timeout(Some(Duration::new(10, 0)));
+        stream.set_nonblocking(true).expect("set_nonblocking call failed");
         // 読み取り。
-        let mut buf = vec![];
-        match stream.read_to_end(&mut buf) {
+        let mut buf = String::new();
+        // FIXME マルチバイト文字の受け取り方が分からん☆（＾～＾）1バイトずつ取る。
+        match stream.take(1).read_to_string(&mut buf) { // stream.read_to_end(&mut buf)
             Ok(len) => {
-                println!("Buf: {}, Len: {}.", String::from_utf8_lossy(buf.as_slice()), len);
                 if 0==len {
                     // 長さが 0 なら切断と判定する。
                     break;
                 }
+                print!("{}", String::from_utf8_lossy(buf.as_bytes()));
+                // println!("Buf: {}, Len: {}.", String::from_utf8_lossy(buf.as_bytes()), len);//buf.as_slice()
             },
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                // wait until network socket is ready, typically implemented
-                // via platform-specific APIs such as epoll or IOCP
-                // wait_for_fd();
-                println!("10 msec wait.");
-                let ten_millis = time::Duration::from_millis(10);
-                thread::sleep(ten_millis);
+                // ブロックしなければ、ここにくる。
+                // print!("[wait]");
+                let seconds = time::Duration::from_millis(1000);
+                thread::sleep(seconds);
             },
             Err(e) => panic!("encountered IO error: {}", e),
         };
@@ -66,7 +67,7 @@ fn main() {
     // ずっと。接続があるたびにループが回る。
     for stream1 in listener.incoming() {
         println!("Incoming 0.");
-        // 別スレッドを立てる。
+        // 別スレッドを立てる。( FIXME スレッドを何個も立てたくないんだが)
         thread::spawn(move || {
             println!("Spawn 0.");
             handle_client(&stream1.unwrap());
