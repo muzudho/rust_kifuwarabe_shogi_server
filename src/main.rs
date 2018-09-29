095ccabf0919b15f416abd0f3afd05776ce8578b
むずでょ <muzudho1@gmail.com>
2018-09-29 17:13:46 +0900

    TCP/IPサーバー部を分離☆（＾～＾）

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@
 /// # コマンド例。
 ///
 /// ```
-/// ### コンパイル(開発中)。 [Windows]+[R]キー, "cmd"+[Enter]。
+/// ###  [Windows]+[R]キー, "cmd"+[Enter]。
 /// cd C:\MuzudhoDrive\projects_rust\rust_kifuwarabe_shogi_server
+///
+/// ### コンパイル(開発中)。
 /// cargo clippy
 ///
 /// ### コンパイル(リリース用)。
@@ -14,121 +16,92 @@
 ///
 /// ### 実行。
 /// cargo run --release
-/// 
+///
 /// ### 開けているポート確認。
 /// netstat
 /// ```
-use std::net::{TcpListener, TcpStream};
-use std::io;
-use std::io::Read;
-use std::{thread, time};
-use std::collections::HashMap;
-#[macro_use]
 extern crate lazy_static;
-use std::time::Duration;
 
+extern crate kifuwarabe_server;
+use kifuwarabe_server::interfaces::*;
+use kifuwarabe_server::*;
 
 const CONNECTION_STRING: &str = "127.0.0.1:4081";
 
-/// クライアントの変数。
-struct ClientVar {
-
-}
-impl ClientVar {
-    pub fn new() -> ClientVar {
-        ClientVar {
-
-        }
-    }
-}
-
-// グローバル変数。
-use std::sync::RwLock;
-lazy_static! {
-    /// スレッド間で共有する。 <接続番号,変数>
-    static ref CLIENT_MAP: RwLock<HashMap<i64, ClientVar>> = RwLock::new(HashMap::new());
-}
-
-
 fn main() {
-    println!("I am a server!");
+    println!("I am a shogi server!");
 
-    // 接続受付スレッド。
-    thread::spawn(move || {
-        println!("Listen!");
-        let mut connection_number = 0;
-        let sever_listener = TcpListener::bind(CONNECTION_STRING).unwrap();
-        for stream_wrap in sever_listener.incoming() {
-            CLIENT_MAP.try_write().unwrap().insert(connection_number, ClientVar::new());
-            thread::spawn(move || {
-                println!("Welcome!");
-                handle_client(connection_number, &stream_wrap.unwrap());
-            });
-            connection_number += 1;
-        }
-    });
+    let server = &Server {
+        receiver: default_receiver,
+    };
 
-    // 各クライアントに何かしたいことがあれば 以下に書く。
-    loop {
-        /*
-        let mut count = 0;
-        match CLIENT_MAP.try_read() {
-            Ok(client_map) => {
-                for (_connection_number, _client_var) in client_map.iter() {
-                    count += 1;
-                }
-            },
-            Err(_) => unreachable!(),
-        };
-        println!("count = {}", count);
-        */
-        thread::sleep(Duration::from_millis(1));
-    }
+    listen(&server, CONNECTION_STRING);
     // サーバーは、[Ctrl]+[C]キーで強制終了しろだぜ☆（＾～＾）
 }
 
+const GAME_ID: &str = "20180929-KFWARABE1-10203";
 
-/// クライアントをずっと捕まえておく。
-fn handle_client(connection_number:i64, stream: &TcpStream) {
-    println!("Capture client. {}", connection_number);
+/**
+ * クライアントからの入力は このメソッド内で処理する。
+ */
+fn default_receiver(req: &Request, res: &mut Response) {
+    println!("<{} {}", req.get_connection_number(), req.get_message());
 
-    // TODO クライアント名を取得したい。
-    // let name = "Kifuwarabe"; // 仮
-
-    // ブロックし続けないようにする。
-    // let _ = stream.set_read_timeout(Some(Duration::new(10, 0)));
-    stream.set_nonblocking(true).expect("set_nonblocking call failed");
-    let mut buf = [0];
-    let mut buf_arr = [0; 1024];
-    let mut index = 0;
-    // FIXME 切断のエラーをキャッチしたい。
-    loop {
-        // 読み取り。
-        // FIXME マルチバイト文字の受け取り方が分からん☆（＾～＾）1バイトずつ取る。
-        match stream.take(1).read(&mut buf) {
-            Ok(len) => {
-                if 0==len {
-                    // 長さが 0 なら切断と判定する。
-                    break;
-                }
-                buf_arr[index] = buf[0];
-                index += 1;
-                if buf[0] == b'\n' {
-                    let line = String::from_utf8_lossy(&buf_arr[0..index]);
-                    print!("{}> {}", connection_number, line); // 改行は line に入っている。
-                    index = 0;
-                }
-
-                // 何か応答したい。
-            },
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                // ブロックしなければ、ここにくる。
-                // print!("[wait]");
-                let msec = time::Duration::from_millis(10);
-                thread::sleep(msec);
-            },
-            Err(e) => panic!("encountered IO error: {}", e),
-        };
+    match req.get_message() {
+        "LOGIN kifuwarabe a" => {
+            res.set_message(&format!(
+                r#"LOGIN:kifuwarabe OK
+BEGIN Game_Summary
+Protocol_Version:1.2
+Protocol_Mode:Server
+Format:Shogi 1.0
+Declaration:Jishogi 1.1
+Game_ID:{}
+Name+:kifuwarabe
+Name-:KITSUNE
+Your_Turn:+
+Rematch_On_Draw:NO
+To_Move:+
+Max_Moves:256
+BEGIN Time
+Time_Unit:1sec
+Total_Time:600
+Byoyomi:10
+Least_Time_Per_Move:1
+END Time
+BEGIN Position
+P1-KY-KE-GI-KI-OU-KI-GI-KE-KY
+P2 * -HI *  *  *  *  * -KA * 
+P3-FU-FU-FU-FU-FU-FU-FU-FU-FU
+P4 *  *  *  *  *  *  *  *  * 
+P5 *  *  *  *  *  *  *  *  * 
+P6 *  *  *  *  *  *  *  *  * 
+P7+FU+FU+FU+FU+FU+FU+FU+FU+FU
+P8 * +KA *  *  *  *  * +HI * 
+P9+KY+KE+GI+KI+OU+KI+GI+KE+KY
+P+
+P-
++
+END Position
+END Game_Summary
+"#, // 最後に改行が必要。
+                GAME_ID
+            ))
+        }
+        r#"
+AGREE"# => {
+            res.set_message(&format!(
+                r#"START:{}
+"#, // 最後に改行が必要。
+                GAME_ID
+            ));
+        }
+        _ => {
+            println!(
+                "<{} Not match: [{}]",
+                req.get_connection_number(),
+                req.get_message()
+            );
+        }
     }
-    println!("Release client.");
 }
